236a135740d3b63ba82a0d85b305990a15915051
John Stewart <jstewartstack@gmail.com>
2022-09-13 19:08:35 +0100

    Create listreview.py

---
+++ lists/listreview.py
@@ -0,0 +1,9 @@
+inventory = ["twin bed", "twin bed", "headboard", "queen bed", "king bed", "dresser", "dresser", "table", "table", "nightstand", "nightstand", "king bed", "king bed", "twin bed", "twin bed", "sheets", "sheets", "pillow", "pillow"]
+
+# Finding out how many items in warehouse inventory
+inventory_len = len(inventory)
+
+# Selecting first element in inventory and saving it to first
+first = inventory[0]
+
+last = inventory[-1]
