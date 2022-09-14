b901290689c206018ac84038ff7ee417f925024f
John Stewart <jstewartstack@gmail.com>
2022-09-14 15:32:16 +0100

    lenslist1

---
+++ lists/lenslist.py
@@ -0,0 +1,15 @@
+# Your code below:
+
+#Make Some Pizza
+
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+#print(num_two_dollar_slices)
+
+num_pizzas = len(toppings)
+#print(num_pizzas)
+
+# John Stewart | lenslist Project | Codecademy | 09/14/2022
