13851b801fb62c1ed8a573ec5d2751b267d3e33f
John Stewart <jstewartstack@gmail.com>
2022-09-14 15:12:19 +0100

    listsreview2

--- lists/listreview.py
+++ lists/listreview.py
@@ -6,4 +6,35 @@ inventory_len = len(inventory)
 # Selecting first element in inventory and saving it to first
 first = inventory[0]
 
+# Selecting the last element from the inventory
 last = inventory[-1]
+
+inventory_2_6 = inventory[2:6]
+
+first_3 = inventory[:3]
+
+twin_beds = inventory.count("twin bed")
+
+removed_item = inventory.pop(4)
+
+inventory.insert(10, "19th Century Bed Frame")
+
+inventory.sort()
+
+inventory = sorted("twin bed")
+print(inventory)
+
+print(removed_item)
+
+print(twin_beds)
+
+
+# John Stewart | Review of Lists Python | Codecademy| 14/09/2022
+
+
+
+
+
+
+
+
