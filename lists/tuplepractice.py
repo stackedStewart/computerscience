a75a1d5536b551d63e19ddaa0289067cf5c089c6
John Stewart <jstewartstack@gmail.com>
2022-09-18 20:44:10 +0100

    loopsreview

---
+++ lists/tuplepractice.py
@@ -0,0 +1,5 @@
+my_info = ('John', 35, 'Programmer')
+print(my_info)
+
+my_info = ('claire')
+print(my_info)
---
+++ loops/loopsreview.py
@@ -0,0 +1,19 @@
+
+single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+squares = []
+
+for digit in single_digits:
+  print(digit)
+  squares.append(digit**2)
+
+print(squares)
+
+cubes = []
+
+for digit in single_digits:
+  cubes.append(digit**3)
+
+print(cubes)
+
+# John Stewart | Python Loops Review | Codecademy | 09/18/2022
