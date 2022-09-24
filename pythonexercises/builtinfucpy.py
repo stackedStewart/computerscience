54f4ed32c694c64d428ed6929e8d90e778aefe0b
John Stewart <jstewartstack@gmail.com>
2022-09-24 15:15:31 +0100

    builtinfuncspy

---
+++ pythonexercises/builtinfucpy.py
@@ -0,0 +1,15 @@
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+
+max_price = max(9.75, 15.50, 5.99, 2.00)
+print(max_price) #Prints out 15.50 as the max price
+
+min_price = min(9.75, 15.50, 5.99, 2.00)
+print(min_price) #Prints out 2.0 as the minimum price
+
+rounded_price = round(tshirt_price, 1)#Rounds the price of tshirt to 1 decimal place
+print(rounded_price)#Prints out the result of the rounded price
