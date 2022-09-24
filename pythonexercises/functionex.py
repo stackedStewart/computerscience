de61218e61d4a5dae53e06f42022baf0087b7a63
John Stewart <jstewartstack@gmail.com>
2022-09-24 14:17:32 +0100

    weathercheck

---
+++ pythonexercises/functionex.py
@@ -0,0 +1,19 @@
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station.")
+  print("Take the Northbound N, Q, R, or W train 1 stop.")
+  print("Get off the Times Square 42nd Street stop.")
+  print("Take lots of pictures!")
+
+# Call your function here:
+
+directions_to_timesSq()
+
+# Your code below: 
+
+print("Checking the weather for you!")
+
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.") # Indent for this to be called along with first statement.
+
+weather_check()
