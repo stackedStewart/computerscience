e5b4fe60b4a60e3c928b64be9387a0481cd91d68
John Stewart <jstewartstack@gmail.com>
2022-09-24 13:50:23 +0100

    loopsexercises

---
+++ pythonexercises/loopex.py
@@ -0,0 +1,12 @@
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"We have {count} even numbers")
+
+count = 0 
+while count <= 3:
+    #count += 1
+    print(count)
+    count += 1 
