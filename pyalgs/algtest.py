87028f354ce225646833550e735ff9b9b9315929
John Stewart <jstewartstack@gmail.com>
2022-10-22 19:22:10 +0100

    finished

--- pyalgs/algtest.py
+++ pyalgs/algtest.py
@@ -1,12 +1,23 @@
+#Create an integer variable for x
 x = 0
+#Create an integer variable for y
 y = 0
+#Create an integer variable for sum
 sum = 0
 
+#Ask user for integer input into ("X: ") and type answer in
 x = int(input("X: "))
+#Ask user for integer input into ("Y: ") and type answer in
 y = int(input("Y: "))
 
+#Sum will equal the two integers of x + y together
 sum = x + y
 
+#Will print out the answer with ("Answer is: ")
 print("Answer is: ")
 print(sum)
 ##print(x + y)
+
+#John Stewart
+#10/22/2022
+#Simple Algorithm test in python
