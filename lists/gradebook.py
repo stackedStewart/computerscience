078ed26d89faa35e9e8b3180cefc1c2807d422d0
John Stewart <jstewartstack@gmail.com>
2023-02-01 20:30:10 +0000

    gradebook finished

--- lists/gradebook.py
+++ lists/gradebook.py
@@ -1,3 +1,5 @@
+#Gradebook project with lists in python
+
 last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
 subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
