3d73da548c4c02f0677bdfff6c80097b0c872592
John Stewart <jstewartstack@gmail.com>
2023-02-09 20:15:11 +0000

    updated

--- loops/loopsreview.py
+++ loops/loopsreview.py
@@ -4,16 +4,16 @@ single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 squares = []
 
 for digit in single_digits:
-  print(digit)
+  #print(digit)
   squares.append(digit**2)
 
-print(squares)
+print("This will print all these digits squared", squares)
 
 cubes = []
 
 for digit in single_digits:
   cubes.append(digit**3)
 
-print(cubes)
+print("These will print all these digits cubed", cubes)
 
-# John Stewart | Python Loops Review | Codecademy | 09/18/2022
+# John Stewart | Python Loops Review | Codecademy | 09/02/2023
